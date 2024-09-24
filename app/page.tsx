c6f88493a5e9d01eb95ff948a668bca17f723641
alex <alex.ivy-townley@stellasource.com>
2024-09-23 21:53:48 -0500

    flight basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import React from 'react';
-import SpaceScene from '@/components/SpaceScene/SpaceScene';
+// import SpaceScene from '@/components/SpaceScene/SpaceScene';
+import SpaceGame from '@/components/SpaceGame/SpaceGame';
+import { Provider } from 'react-redux';
+import store from './state/store';
 
 const HomePage = () => {
   return (
-    <div>
-      <SpaceScene />
-    </div>
+    <Provider store={store}>
+
+      {/* <SpaceScene /> */}
+      <SpaceGame />
+    </Provider>
   );
 };
 
